05acddee3f8bc34828dd53424d2aef402ec38421
Batmunkh Erdenejargal <erdenejargal@Batmunkhs-Mac-mini.local>
2018-10-26 22:22:18 +0800

    backend setup

---
+++ server/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2017
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        'no-console': 'off'
+    }
+};
